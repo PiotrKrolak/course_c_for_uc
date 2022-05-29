beffd28657099f526877d600a6513c9c436008c9
PiotrKrolak <krolak.piotr.lca@gmail.com>
2022-05-29 23:14:01 +0200

    dyrektywy preprocesora - definiowanie prostych finkcji - praktyczne wykorzystanie2

---
+++ modul1/dyrektywy_preprocesora2.c
@@ -0,0 +1,16 @@
+// cwiczenie dyrektyw preprocesora #if, #ifdef etc...
+
+#include <stdio.h>
+
+#define flaga "abc"
+
+// #if defined flaga
+//     #define kolor "czerwoni"
+// #else   
+//     #define kolor "niebiescy"
+
+int main(){
+    printf(flaga);
+
+    return 0;
+}
