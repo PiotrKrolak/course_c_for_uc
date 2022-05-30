0ed2005ce5283a3be1ec5942fd750e397c95fd56
PiotrKrolak <krolak.piotr.lca@gmail.com>
2022-05-30 22:36:04 +0200

    flagi optymalizacji -o0, -o1, -o2 oraz zadanie SPOJ: FCTRL3

---
+++ modul1/optymalizacja.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define kwadrat(x) ((x)*(*))
+#define pi 3,14
+
+int main(){
+    double *promien;
+
+    scanf("%lf", promien);
+
+    printf(".2lf",2*pi*((*promien) * (*promien)));
+
+    return 0;
+}
